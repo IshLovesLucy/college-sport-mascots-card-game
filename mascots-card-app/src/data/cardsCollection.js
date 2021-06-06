44cec205cfbd7276035121ea7e4e7981abe6ccae
Ismail Khair <ismailkhair@ismails-mbp.myfiosgateway.com>
2021-06-06 11:31:40 -0400

    add: WIP static data

---
+++ mascots-card-app/src/data/cardsCollection.js
@@ -0,0 +1,20 @@
+/* eslint no-unused-vars: 0 */
+
+/*
+  A list of Cards
+*/
+
+module.export = [
+  { id: 'card01', url: '/src/img-ny-binghamton-bearcats.png' },
+  { id: 'card02', url: '/src/img-ny-bmcc.png' },
+  { id: 'card03', url: '/src/img-ny-bronx-community.png' },
+  { id: 'card04', url: '/src/img-ny-c1.png' },
+  { id: 'card05', url: '/src/img-ny-canisius.png' },
+  { id: 'card06', url: '/src/img-ny-clarkson.png' },
+  { id: 'card07', url: '/src/img-ny-colgate.png' },
+  { id: 'card08', url: '/src/img-ny-cortland.png' },
+  { id: 'card09', url: '/src/img-ny-rit.png' },
+  { id: 'card10', url: '/src/img-ny-rutgers.png' },
+  { id: 'card11', url: '/src/img-ny-st-johns.png' },
+  { id: 'card12', url: '/src/img-ny-syracuse.png' },
+];
